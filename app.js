5dfdc6b1964f2ffa0ef7af02f75654cce3c3c809
pereztjacob <pereztjacob@gmail.com>
2017-10-31 17:17:06 -0700

    displays table with cookie stand sales

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 'use strict';
 
- const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
-        '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm',
+    '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-function render(n){
-    const ul = document.getElementById('whatever');
-    const li = document.createElement('li');
-    li.textContent = hours[n] + ': ' + sphAirport[n + 1] + ' cookies';
-    ul.appendChild(li);
+function renderHours(n){
+    const hrs = document.getElementById('firstLine');
+    const td = document.createElement('td');
+    td.textContent = hours[n];
+    hrs.appendChild(td);
+}
+
+function render(n, parent, sph){
+    let tr = document.getElementById(parent);
+    let td = document.createElement('td');
+    td.textContent = sph[n];
+    tr.appendChild(td);
 }
 
 function custPerHour(minCustomers, maxCustomers){
@@ -17,7 +24,7 @@ function custPerHour(minCustomers, maxCustomers){
 };
 
 function salesPerHour(avgSales, custPerHour){
-    return avgSales * custPerHour;
+    return Math.floor(avgSales * custPerHour);
 };
 
 function Store(minCustomers, maxCustomers, avgSales, custPerHour, salesPerHour, render){
@@ -29,95 +36,28 @@ function Store(minCustomers, maxCustomers, avgSales, custPerHour, salesPerHour, 
     this.render = render;
 };
 
-const pdxAirport = new Store()
+const pdxAirport = new Store(23, 65, 6.3, custPerHour, salesPerHour, render);
+const pioneerSq = new Store(3, 24, 1.2, custPerHour, salesPerHour, render);
+const powells = new Store(11, 38, 3.7, custPerHour, salesPerHour, render);
+const stJohns = new Store(20, 28, 2.3, custPerHour, salesPerHour, render);
+const waterfront = new Store(2, 16, 4.6, custPerHour, salesPerHour, render);
 
-let cphAirport = [custPerHour(pdxAirport.minCustomers, pdxAirport.maxCustomers)];
-let sphAirport = [parseInt((pdxAirport.salesPerHour(pdxAirport.avgSales, cphAirport)))];
-for(let i = 0; i < 14; i++){
-    cphAirport.push(custPerHour(pdxAirport.minCustomers, pdxAirport.maxCustomers));
-};
-console.log(cphAirport);
-for(let n = 0; n < 14; n++){
-    sphAirport.push(parseInt(salesPerHour(pdxAirport.avgSales, cphAirport[n])));
-    render(n);
-};
-console.log(sphAirport);
+function calculateCookies(object, line){
+    const airportArray = [];
+    for(let i = 0; i < hours.length; i++){
+        const cph = custPerHour(object.minCustomers, object.maxCustomers);
+        const sph = object.salesPerHour(object.avgSales, cph);
+        airportArray.push(sph);
+        render(i, line, airportArray);
+    };
+    console.log(airportArray);
+}
 
-const pioneerSq = {
-    minCustomers: 3,
-    maxCustomers: 24,
-    avgSales: 1.2,
-    custPerHour: custPerHour,
-    salesPerHour: salesPerHour
-};
-
-let cphPioneer = [custPerHour(pioneerSq.minCustomers, pioneerSq.maxCustomers)];
-let sphPioneer = [parseInt((pioneerSq.salesPerHour(pioneerSq.avgSales, cphPioneer)))];
-for(let i = 1; i < 14; i++){
-    cphPioneer.push(custPerHour(pioneerSq.minCustomers, pioneerSq.maxCustomers));
-};
-console.log(cphAirport);
-for(let n = 1; n < 14; n++){
-    sphPioneer.push(parseInt(salesPerHour(pioneerSq.avgSales, cphAirport[n])));
-};
-console.log(sphPioneer);
-
-const powells = {
-    minCustomers: 11,
-    maxCustomers: 38,
-    avgSales: 3.7,
-    custPerHour: custPerHour,
-    salesPerHour: salesPerHour
-};
-
-let cphPowells = [custPerHour(powells.minCustomers, powells.maxCustomers)];
-let sphPowells = [parseInt((powells.salesPerHour(powells.avgSales, cphPowells)))];
-for(let i = 1; i < 14; i++){
-    cphPowells.push(custPerHour(powells.minCustomers, powells.maxCustomers));
-};
-console.log(cphPowells);
-for(let n = 1; n < 14; n++){
-    sphPowells.push(parseInt(salesPerHour(powells.avgSales, cphPowells[n])));
-};
-console.log(sphPowells);
-
-const stJohns = {
-    minCustomers: 20,
-    maxCustomers: 38,
-    avgSales: 2.3,
-    custPerHour: custPerHour,
-    salesPerHour: salesPerHour
-};
-
-let cphStJohns = [custPerHour(stJohns.minCustomers, stJohns.maxCustomers)];
-let sphStJohns = [parseInt((stJohns.salesPerHour(stJohns.avgSales, cphStJohns)))];
-for(let i = 1; i < 14; i++){
-    cphStJohns.push(custPerHour(stJohns.minCustomers, stJohns.maxCustomers));
-};
-console.log(cphStJohns);
-for(let n = 1; n < 14; n++){
-    sphStJohns.push(parseInt(salesPerHour(stJohns.avgSales, cphStJohns[n])));
-};
-console.log(sphStJohns);
-
-const waterfront = {
-    minCustomers: 2,
-    maxCustomers: 16,
-    avgSales: 4.6,
-    custPerHour: custPerHour,
-    salesPerHour: salesPerHour
-};
-
-let cphWaterfront = [custPerHour(waterfront.minCustomers, waterfront.maxCustomers)];
-let sphWaterfront = [parseInt((waterfront.salesPerHour(waterfront.avgSales, cphWaterfront)))];
-for(let i = 1; i < 14; i++){
-    cphWaterfront.push(custPerHour(waterfront.minCustomers, waterfront.maxCustomers));
-};
-console.log(cphWaterfront);
-for(let n = 1; n < 14; n++){
-    sphWaterfront.push(parseInt(salesPerHour(waterfront.avgSales, cphWaterfront[n])));
-};
-console.log(sphWaterfront);
-
-
-
+for(let i = 0; i < 15; i++){
+    renderHours(i);
+}
+calculateCookies(pdxAirport, 'secondLine');
+calculateCookies(pioneerSq, 'thirdLine');
+calculateCookies(powells, 'fourthLine');
+calculateCookies(stJohns, 'fifthLine');
+calculateCookies(waterfront, 'sixthLine');
